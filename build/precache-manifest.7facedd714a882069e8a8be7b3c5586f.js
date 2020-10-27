ab53687959e937697719867ecea29e159b5e0628
Ubuntu <ubuntu@ip-172-31-5-107.ap-south-1.compute.internal>
2020-10-27 16:45:28 +0000

    Added appspeck.yml file

---
+++ build/precache-manifest.7facedd714a882069e8a8be7b3c5586f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fad11015e3e5817ddc376698713a6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b856014226976193ca6",
+    "url": "/static/css/main.9c094753.chunk.css"
+  },
+  {
+    "revision": "076132238b67c745c9e9",
+    "url": "/static/js/2.fbd9dd52.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/static/js/2.fbd9dd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b856014226976193ca6",
+    "url": "/static/js/main.b4da1f3f.chunk.js"
+  },
+  {
+    "revision": "4390a351b0f90a7c34c6",
+    "url": "/static/js/runtime-main.8bc1365d.js"
+  }
+]);
---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+#sudo pm2 serve build/ 3000
+echo "Hello from shell script starting"
