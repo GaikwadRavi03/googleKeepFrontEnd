f438bbfae3773d8c296c6fbdf425e183dc451532
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-10-29 19:59:26 +0530

    Update start.sh
    refactored : start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu/client
+echo "===========ls=============start==========="
+ls
 sudo npm run build
 echo "hello Ravi this is ***************start*********** script"
 sudo pm2 serve build/ 3000
