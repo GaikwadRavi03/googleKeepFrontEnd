63af950b8f6cf8dfe89f07e1917308d6f273cc5f
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-10-29 23:28:24 +0530

    Update update.sh
    remove update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 echo "Hello Ravi this is ************update**************** script here"
-cd /home/ubuntu/client
-sudo npm i
-echo "----------------------npm build----------------------------"
-sudo npm run build
