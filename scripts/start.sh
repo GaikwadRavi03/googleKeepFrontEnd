c3df450d120fc209b88090e35c6762439d0b66b7
Ubuntu <ubuntu@ip-172-31-5-107.ap-south-1.compute.internal>
2020-10-29 04:38:35 +0000

    Added : new version of title name

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,6 @@
+#!/bin/bash
 
-echo "Hello from shell script starting"
 cd /home/ubuntu/client/
 sudo npm run build
 
-sudp pm2 kill
 sudo pm2 serve build/ 3000
-myPm2=`pm2 startup` 
-$myPm2
-pm2 save
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get upgrade -y
+
+cd /home/ubuntu/client/
 git pull origin master
 git status
 
