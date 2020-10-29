17eaaa3051c8106fd8a66ee97b9ce0edcf8c18ac
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-10-29 20:38:59 +0530

    Update start.sh
    reafctored : remove pm2

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu/client
 sudo npm i
 echo "===========ls=============start==========="
 ls
-sudo npm run build
+#sudo npm run build
 echo "hello Ravi this is ***************start*********** script"
-sudo pm2 serve build/ 3000
+#sudo pm2 serve build/ 3000
