c82b2a78f8b06f5f8acb02a0d92d364fb6cb563a
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-10-29 23:25:21 +0530

    Update update.sh
    refactored : update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Hello Ravi this is ************update**************** script here"
 cd /home/ubuntu/client
-#sudo npm i
-echo "----------------------pull Request----------------------------"
-#git pull origin master
-#sudo npm run build
+sudo npm i
+echo "----------------------npm build----------------------------"
+sudo npm run build
