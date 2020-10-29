1bff9a98a42caf7adf4d3a9872514196c322d7e3
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-10-29 23:09:47 +0530

    Update update.sh
    comment some line

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,5 +3,5 @@ echo "Hello Ravi this is ************update**************** script here"
 cd /home/ubuntu/client
 #sudo npm i
 echo "----------------------pull Request----------------------------"
-git pull origin master
-sudo npm run build
+#git pull origin master
+#sudo npm run build
